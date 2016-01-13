8346fe00575ccaa3e90827b9cbebcb2d6d6b6e2c
slawiko <shchaurouski.slava@gmail.com>
2016-01-13 21:16:48 +0300

    Working with 4th task is over

--- Lesson4_Shchaurouski/controller/App.controller.js
+++ Lesson4_Shchaurouski/controller/App.controller.js
@@ -1,9 +1,41 @@
 sap.ui.define([
-	'sap/ui/core/mvc/Controller'
-], function (Controller) {
+	'sap/ui/core/mvc/Controller',
+	'sap/m/Dialog',
+	'sap/m/Button',
+	'sap/ui/model/json/JSONModel'
+], function (Controller, Dialog, Button, JSONModel) {
 	'use strict';
+	var counter = 0;
 
 	return Controller.extend('leverx.sap.shchaurouski.fourth.controller.App', {
+		handleShowDialogPress: function (oEvent) {
+			counter++;
 
+			var dialog = new Dialog({
+				title: 'Dialog opened ' + counter + ' times',
+				type: 'Message',
+				content: (function () {
+					var dialogFragment = sap.ui.xmlfragment('leverx.sap.shchaurouski.fourth.view.DialogContent'),
+						oData = {
+							message: 'Test message'
+						},
+						oModel = new JSONModel(oData);
+
+					dialogFragment.setModel(oModel);
+					return dialogFragment;
+				})(),
+				beginButton: new Button({
+					text: 'Close',
+					press: function () {
+						dialog.close();
+					}
+				}),
+				afterClose: function () {
+					dialog.destroy();
+				}
+			});
+
+			dialog.open();
+		}
 	});
 });

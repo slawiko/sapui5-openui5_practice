213402e5cec1c98bc071f76f8dae4ae271313c8c
slawiko <shchaurouski.slava@gmail.com>
2016-01-15 14:54:06 +0300

    T-5: bug fix

--- Lesson5_Shchaurouski/controller/App.controller.js
+++ Lesson5_Shchaurouski/controller/App.controller.js
@@ -1,11 +1,43 @@
 sap.ui.define([
-	'sap/ui/core/mvc/Controller'
-], function (Controller) {
+	'sap/ui/core/mvc/Controller',
+	'sap/m/Dialog',
+	'sap/m/Button',
+	'sap/ui/model/json/JSONModel'
+], function (Controller, Dialog, Button, JSONModel) {
 	"use strict";
 	return Controller.extend('leverx.sap.shchaurouski.fifth.controller.App', {
 		onInit: function () {
 			var oModel = new sap.ui.model.odata.ODataModel('/sap/opu/odata/', true);
 			this.getView().setModel(oModel);
+		},
+		onReviewPress: function (oEvent) {
+			var productId = oEvent.getSource().getBindingContext().getProperty('Id'),
+				dialog = new Dialog({
+					title: productId,
+					content: (function () {
+						var dialogFragment = sap.ui.xmlfragment('leverx.sap.shchaurouski.fifth.view.DialogContent'),
+							i,
+							oModel = new sap.ui.model.odata.ODataModel('/sap/opu/odata/', true);
+
+						// ************
+
+
+						// ************
+						dialogFragment.setModel(oModel);
+						return dialogFragment;
+					})(),
+					beginButton: new Button({
+						text: 'Close',
+						press: function () {
+							dialog.close();
+						}
+					}),
+					afterClose: function () {
+						dialog.destroy();
+					}
+				});
+
+			dialog.open();
 		}
 	});
 });
